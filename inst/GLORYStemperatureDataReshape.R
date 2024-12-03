94e720076bd85430a1d80492f12e1135f5de242f
Cook <Adam.Cook@dfo-mpo.gc.ca>
2024-12-03 11:39:16 -0400

    update

--- inst/GLORYStemperatureDataReshape.R
+++ inst/GLORYStemperatureDataReshape.R
@@ -1,31 +1,19 @@
 
 require(bio.lobster)
+require(bio.lobster.glorys)
 require(satin)
 require(tidyr)
-setwd(file.path(project.datadirectory('bio.lobster.glorys'),'Downloads'))
 
-y1 = read.cmems('GLORYS2020-01-01.nc')
+setwd(file.path(project.datadirectory('bio.lobster.glorys')))
+fil = dir('Downloads')
+y1 = read.cmems(file.path('Downloads',fil[2]))
 a = y1$bottomT
-image(a@lon, a@lat, t(a@data[,,1,]))
-
-#LFA 36
-fil = dir()
-fil = fil[grep('GLO',fil)]
-	LFAs<-read.csv(file.path( project.datadirectory("bio.lobster"), "data","maps","LFAPolys.csv"))
-	L = subset(LFAs, PID %in% 34:38)
-
-
-for(i in 1:length(fil)){
-		g = glorysSubset(glorysfile=fil[i], polygon=L)
-		saveRDS(g,file = file.path('SummaryFiles',paste(fil[i],'LFA3438.rds',sep="_")))
-	}
-
+image(a@lon, a@lat, t(a@data[,,300,]))
 
 #glorys reshape to r Object
-fil = fil[grep('int',fil)]
 
 for(i in 1:length(fil)){
-		g = glorysReshape(glorysfile=fil[i])
+		g = glorysReshapeLong(glorysfile=file.path('Downloads',fil[i]))
 		saveRDS(g,file = file.path('SummaryFiles',paste(fil[i],'ShelfBoF.rds',sep="_")))
 		print(fil[i])
 	}
